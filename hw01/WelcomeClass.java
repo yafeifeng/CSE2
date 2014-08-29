3ced7b8beb31a19e3615cb6a24eda176534aad6a
yafei <feeyavon@gmail.com>
2014-08-29 15:31:33 +0000

    updated comment

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+/////////////////////////////
+//Yafei Feng
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//  java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+      System.out.println("-----------");
+      System.out.println("| WELCOME |");
+      System.out.println("-----------");
+      System.out.println("^  ^  ^  ^  ^  ^");
+      System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+      System.out.println("|-Y--A--F--2--1--7-|");
+      System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+      System.out.println("v  v  v  v  v  v");
+      System.out.println("This summer I participated in the summer exchange program in HKUST, which is in HK.It was a pleasure spending my summer holiday there.");
+      System.out.println("test");
+      
+      //test/testtest
+  }
+}
